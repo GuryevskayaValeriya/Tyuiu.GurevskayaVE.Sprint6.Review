23196127ac79fc9def847175b522596585b5d4f4
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-12-13 11:53:11 +0500

    fix (test)

--- Tyuiu.GurevskayaVE.Sprint6.Review.V28.Test/DataServiceTest.cs
+++ Tyuiu.GurevskayaVE.Sprint6.Review.V28.Test/DataServiceTest.cs
@@ -11,13 +11,12 @@ namespace Tyuiu.GurevskayaVE.Sprint6.Review.V28.Test
         public void ValidGetMatrix()
         {
             DataService ds = new DataService();
-            int[,] mas = new int[5, 5] { { -4, -3, -2, -1, 0 }, 
-                                         { 1, 2, 3, 4, 0 }, 
-                                         { -4, -3, -2, -1, 0 }, 
-                                         { 1, 2, 3, 4, 0 }, 
-                                         { -4, -3, -2, -1, 0 } };
-            double res = ds.GetMatrix(mas, -4, 5, 1, 1, 2);
-            double wait = -0.6;
+            int[,] mas = new int[3, 3] { { -4, -3, -2 },
+                                         { 1, 2, 3 },
+                                         { -4, -3, -2 } };
+                                         
+            double res = ds.GetMatrix(mas, -4, 5, 1, 2, 2);
+            double wait = -1;
 
             Assert.AreEqual(wait, res);
         }
@@ -30,7 +29,7 @@ namespace Tyuiu.GurevskayaVE.Sprint6.Review.V28.Test
                                          { 1, 2, 4, 5 },
                                          { 3, 4, 5, 5 } };
             double res = ds.GetMatrix(arr, 1, 6, 0, 1, 2);
-            double wait = 2.917;
+            double wait = 3.3;
             Assert.AreEqual(wait, res);
 
         }
